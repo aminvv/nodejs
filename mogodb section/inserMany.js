7c5c7b66434860a927f5472f247376c443855b50
aminvv <aminviper1378@gmail.com>
2025-04-15 13:01:46 +0330

    mongodn at insertMany sec:109

---
+++ mogodb section/inserMany.js
@@ -0,0 +1,61 @@
+const { MongoClient } = require("mongodb");
+const DB_URL = "mongodb://localhost:27017"
+const DB_NAME = "mongo-tutorial"
+const client = new MongoClient(DB_URL)
+async function main() {
+    await client.connect()
+    console.log(" connection to mongodb");
+    const db = client.db(DB_NAME)
+    const userCollection = db.collection("user")
+    result = await userCollection.insertMany(
+        [
+            {
+                firstName: "amin",
+                lastName: "asadi",
+                age: 26,
+                skills: ["back-ens", "NodeJs", "Mongodb", "Typescript", "NestJs"],
+                identity: '2741572496',
+                birthday: new Date("09-09-1999"),
+                address: {
+                    province: "azerbaijan",
+                    city: "urmia",
+                    street: "alamemajlesi"
+                }
+            },
+            {
+                firstName: "ali",
+                lastName: "asadi",
+                age: 26,
+                skills: ["back-ens", "NodeJs", "Mongodb", "Typescript", "NestJs"],
+                identity: '2741572496',
+                birthday: new Date("09-09-1999"),
+                address: {
+                    province: "azerbaijan",
+                    city: "urmia",
+                    street: "alamemajlesi"
+                }
+            },
+            {
+                firstName: "hasan",
+                lastName: "asadi",
+                age: 26,
+                skills: ["back-ens", "NodeJs", "Mongodb", "Typescript", "NestJs"],
+                identity: '2741572496',
+                birthday: new Date("09-09-1999"),
+                address: {
+                    province: "azerbaijan",
+                    city: "urmia",
+                    street: "alamemajlesi"
+                }
+            }
+        ]
+    )
+
+    userCollection.countDocuments({}).then(count=>{
+        console.log("count data.......>>>>",count);
+        
+    })
+
+
+}
+main()  
