4ff6c87283029bd6ed07d42d85fd65fadafe16b1
aminvv <aminviper1378@gmail.com>
2025-04-15 12:40:15 +0330

    mongodb at insertOne sec:108

--- mogodb section/server.js
+++ mogodb section/server.js
@@ -7,6 +7,20 @@ async function main() {
     console.log(" connection to mongodb");
     const db=client.db(DB_NAME)
     const userCollection=db.collection("user")
+    result =await userCollection.insertOne({
+         firstName:"amin",
+         lastName:"asadi",
+         age:26,
+         skills:["back-ens","NodeJs","Mongodb","Typescript","NestJs"],
+         identity:'2741572496',
+         birthday:new Date("09-09-1999"),
+         address:{
+            province:"azerbaijan",
+            city:"urmia",
+            street:"alamemajlesi"
+
+         }
+    })
 
 }
 main() 
