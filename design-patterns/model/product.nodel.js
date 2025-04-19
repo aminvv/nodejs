5d8f5e49d9a8528d74570d562f9a977f90660d46
aminvv <aminviper1378@gmail.com>
2025-04-19 17:02:32 +0330

    mini project  manegment product part 2 sec:122

--- design-patterns/model/product.nodel.js
+++ design-patterns/model/product.nodel.js
@@ -1,10 +1,10 @@
 const { ObjectId } = require('bson')
 const{connectToMongoDb} =require('../utils/mongodb-connection.js')
-
+ const productCollection="product"
 async function find(){
     const db=await new connectionToMongodb().Get()
     return new Promise(async(resolve,reject)=>{
-        const products=await db.collection("product").find({}).toArray(
+        const products=await db.collection(productCollection).find({}).toArray(
             resolve(products)
         )
     })
@@ -15,7 +15,44 @@ async function find(){
 async function FindOneId(id){
     const db=await new connectToMongoDb().Get()
     return new Promise(async(resolve,reject)=>{
-        const product=await db.collection("product").findOne({_id: new ObjectId(id)})
+        const product=await db.collection(productCollection).findOne({_id: new ObjectId(id)})
         resolve(product)
     })
+}
+
+
+async function create (product){
+    const db=await new connectionToMongodb().Get()
+    return new Promise(async(resolve,reject)=>{
+        const result= await db.collection(productCollection).insertOne(product)
+        resolve(result)
+    })
+}
+
+
+
+async function update (id,payload){
+    const db=await new connectionToMongodb().Get()
+    return new Promise(async(resolve,reject)=>{
+        const result= await db.collection(productCollection).update({_id: new ObjectId(id)},{$set:{...payload}})
+        resolve(result)
+    })
+}
+
+
+
+async function remove (id){
+    const db=await new connectionToMongodb().Get()
+    return new Promise(async(resolve,reject)=>{
+        const result= await db.collection(productCollection).deleteOne({_id: new ObjectId(id)})
+        resolve(result)
+    })
+}
+
+const productModel={
+    find,
+    FindOneId,
+    create,
+    update,
+    remove,
 }
