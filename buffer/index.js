34e99ce6864c02ab95d942989fbfe5621e1f9047
aminvv <aminviper1378@gmail.com>
2025-04-20 19:09:58 +0330

    buffer data type sec:128

---
+++ buffer/index.js
@@ -0,0 +1,24 @@
+//  const buff=Buffer.from("NodeJS")   حافظه به تعداد ورودی تصخیص داد شده
+//  console.log(buff);
+//  console.log(buff[0]);
+//  console.log("N".codePointAt(0));
+//  console.log("N".charCodeAt(0));
+//  console.log("N".charCodeAt(0).toString(16));
+//  console.log(buff.toString());
+
+
+
+
+//  let buff=Buffer.alloc(4) 4 تصخیص داد شده
+//  buff.write("Node")
+
+//  buff[1]=110 ----->>>>>> Nnode
+
+//  console.log(buff);
+//  console.log(buff[0]);
+//  console.log("N".codePointAt(0));
+//  console.log("N".charCodeAt(0));
+//  console.log("N".charCodeAt(0).toString(16));
+//  console.log(buff.toString());
+ 
+ 
